55bd71997d28ae7562bb456b4fbd6e19ca82f332
Pratik Mukerji <pratik@electricfeel.com>
2016-01-08 22:42:00 +0100

    Extract log preamble and fix typo
    Also favours if->else over if->next->statement

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -16,16 +16,19 @@ class Teacher < User
 
   def self.conditionally_send_journals
     not_before = Time.now - 1.day
-    logger.info "Beginning journal deliver run, refernce time #{not_before}"
+    logger.info "Beginning journal deliver run, reference time #{not_before}"
+
     Teacher.active.where(receive_journals: true).find_each do |teacher|
-      logger.info "[#{teacher.id}] #{teacher.display_name}: checking journals"
+      log_preamble = "[#{teacher.id}] #{teacher.display_name}: "
+      logger.info log_preamble + 'checking journals'
+
       journals = teacher.todays_journals(not_before)
       if journals.empty?
-        logger.info "[#{teacher.id}] #{teacher.display_name}: no new journals"
-        next
+        logger.info log_preamble + 'no new journals'
+      else
+        logger.info log_preamble + "sending #{journals.size} journals"
+        Notifications.journals_created(teacher, journals).deliver_now
       end
-      logger.info "[#{teacher.id}] #{teacher.display_name}: sending #{journals.size} journals"
-      Notifications.journals_created(teacher, journals).deliver_now
     end
   end
 
